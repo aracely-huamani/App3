7746ab4b9287cf4f814953dc8e63bf0eb033832d
aracely-huamani <102335298+aracely-huamani@users.noreply.github.com>
2023-06-03 18:44:48 -0500

    Agregar archivos de proyecto.

---
+++ App3/App3/MonkeysPageViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace App3
+{
+    public class MonkeysPageViewModel : ViewModelBase
+    {
+        public IList<Monkey> Monkeys { get { return MonkeyData.Monkeys; } }
+
+        Monkey selectedMonkey;
+        public Monkey SelectedMonkey
+        {
+            get { return selectedMonkey; }
+            set
+            {
+                if (selectedMonkey != value)
+                {
+                    selectedMonkey = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+    }
+
+}
